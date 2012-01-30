9177fcb4deeec1690cae7de1535f848ef5f32989
Kurt Schrader <kurt@intentmedia.com>
2012-01-29 20:56:18 -0500

    Load the username/password from a property file.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [http.async.client "0.4.0"]
-                 [cheshire "2.1.0"]])
+                 [cheshire "2.1.0"]
+                 [propertea "1.2.3"]])
--- src/stormy/core.clj
+++ src/stormy/core.clj
@@ -1,15 +1,18 @@
 (ns stormy.core
-  (:require
-    [http.async.client :as client]
-    [cheshire.core :as json]))
+  (:require [http.async.client :as client])
+  (:use
+    [cheshire.core]
+    [propertea.core]))
+
+(def props (read-properties "twitter.properties"))
 
 (def url "https://stream.twitter.com/1/statuses/sample.json")
-(def cred {:user "kurt" :password "password"})
+(def cred {:user (:username props) :password (:password props)})
 
 (with-open [client (client/create-client)] ;; Create client
   (let [resp (client/stream-seq client :get url :auth cred) ; The http response
         strings (client/string resp)] ; Turn the response into a string
     (doseq [part strings] ; Since this is a stream, will keep adding new tweets
-        (println (:text (json/parse-string part true)))))) ; Print the tweet
+        (println (:text (parse-string part true)))))) ; Print the tweet
 
 
